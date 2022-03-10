4beb3f24cf3e3a82bb2517abcedaef163d1c81ac
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-03-10 14:19:31 -0500

    Got React to render.

--- code_seonbi_main/code_seonbi_main/settings.py
+++ code_seonbi_main/code_seonbi_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.apps.BackendConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- code_seonbi_main/frontend/src/App.js
+++ code_seonbi_main/frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component, createRef } from 'react'
-import './App.css';
+import React, { Component, createRef } from 'react';
+import { render } from "react-dom";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,10 +7,11 @@ import {
   Link,
   Redirect
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/Nav/Nav.jsx'
-import Home from './pages/Index/index.jsx'
-import Error404 from './pages/Error404/error404.jsx'
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Nav from './components/Nav/Nav.jsx'
+// import Home from './pages/Index/index.jsx'
+// import Error404 from './pages/Error404/error404.jsx'
 
 class App extends Component {
   state = {
@@ -20,18 +21,22 @@ class App extends Component {
   render() {
     return (
       <div> 
-        <Router>
+        {/* <Router> */}
           {/* Use Switch to avoid 404 not rendering on all pages. */}
-          <Switch>
+          {/* <Switch> */}
             {/*All our Routes goes here!*/}
-            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/" component={Home} />
             <Route component={Error404} />
           </Switch>
-        </Router>      
+        </Router>       */}
+        <h1>Hello!</h1>
       </div>
 
     )
   }
 }
+
+const altRootDiv = document.getElementById('atlRoot');
+  render(<App />, altRootDiv);
 
 export default App;
