e5041920e2fc6a1073317c509feb7fc2189d1e60
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-03-15 12:44:31 -0400

    GoGot map to work.

--- code_seonbi_main/frontend/src/components/Test/Test.jsx
+++ code_seonbi_main/frontend/src/components/Test/Test.jsx
@@ -4,30 +4,21 @@ import {Navbar} from 'react-bootstrap';
 
 class Test extends Component {
     state = {
-        id: undefined,
-        project: undefined,
-        technology: undefined,
-        status: undefined,
-        description: undefined,
-        error: undefined,
-        links: undefined,
-        solution: undefined,
-        timestamp: undefined,
+        //  Temporary array before JSON data mapped from fetch.
+        newData: []
     }
 
+    // Function: Sets state to data from backend Bug model.
+    // Need arrow function to use setState.
     getBugList = () => {
         fetch("/backend/bug-list")
             .then((response) => response.json())
             .then((data) => {
                 this.setState ({
-                    id: JSON.stringify(data[0].id), 
-                    project: JSON.stringify(data[0].project), 
-                    technology: JSON.stringify(data[0].technology),
-                    description: JSON.stringify(data[0].description),
-                    timestamp: JSON.stringify(data[0].timestamp),
+                    // "Bug" model data.
+                    newData: data
                 })
             })
-            // .then(data => console.log(data[0]));
     }
 
     render() {
@@ -35,15 +26,19 @@ class Test extends Component {
             <div>
                 <button onClick={this.getBugList}>GET Request</button>
                 <br></br>
-                <p>ID: {this.state.id}</p>
-                <p>Project: {this.state.project}</p>
-                <p>Technology: {this.state.technology}</p>
-                <p>Status: {this.state.status}</p>
-                <p>Description: {this.state.description}</p>
-                <p>Error: {this.state.error}</p>
-                <p>Links: {this.state.links}</p>
-                <p>Solution: {this.state.solution}</p>
-                <p>Timestamp: {this.state.timestamp}</p>
+                { this.state.newData.map((list) => (
+                    <div>
+                        <p>ID: {JSON.stringify(list.id)}</p>
+                        <p>Project: {JSON.stringify(list.project)}</p>
+                        <p>Status: {this.state.status}</p>
+                        <p>Description: {this.state.description}</p>
+                        <p>Error: {this.state.error}</p>
+                        <p>Links: {this.state.links}</p>
+                        <p>Solution: {this.state.solution}</p>
+                        <p>Timestamp: {this.state.timestamp}</p>
+                    </div>
+                ))}
+
             </div>
         )
     }
