fcc66f2b22798b35d37a9a8f82946f3842d585bb
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-06-18 15:08:08 -0400

    Added .env and changed SECRE_KEY.

--- code_seonbi_main/code_seonbi_main/settings.py
+++ code_seonbi_main/code_seonbi_main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# To .env.
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qg!w!g&zv37sm+y&c!@ur0z0vy+$))hp7413-tle4u=dt95xl@'
+# SECRET_KEY = 'django-insecure-qg!w!g&zv37sm+y&c!@ur0z0vy+$))hp7413-tle4u=dt95xl@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
