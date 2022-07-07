a941a5952850ec0573a2e0b08fb1715531a1e181
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-07-07 10:18:35 -0400

    Changed wsgi.py to os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_seonbi_main.code_seonbi_main.settings') .

--- code_seonbi_main/code_seonbi_main/settings.py
+++ code_seonbi_main/code_seonbi_main/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'https://code-seonbi.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'code-seonbi.herokuapp.com']
 
 
 # Application definition
