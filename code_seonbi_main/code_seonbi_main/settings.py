6f5caac9f834e6dd9b751ac415b8c09c43b1527b
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-07-07 13:48:28 -0400

    Test.

--- code_seonbi_main/code_seonbi_main/settings.py
+++ code_seonbi_main/code_seonbi_main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 
