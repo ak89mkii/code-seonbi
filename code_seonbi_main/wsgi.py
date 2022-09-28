c88031530e3d9bcc806b305756dae88987aa5222
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-09-28 15:27:30 -0400

    Modified settings.py static settings for deployment.

--- code_seonbi_main/wsgi.py
+++ code_seonbi_main/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_seonbi_main.code_seonbi_main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'code_seonbi_main.settings')
 
 application = get_wsgi_application()
