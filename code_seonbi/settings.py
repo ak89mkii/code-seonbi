b6f56e0b8a8022e47d38e520e49acd54e8d73ff0
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-10-15 14:08:31 -0400

    Added copy functionality.

--- code_seonbi/settings.py
+++ code_seonbi/settings.py
@@ -28,7 +28,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'code-seonbi.herokuapp.com']
 ALLOWED_HOSTS = ['*']
