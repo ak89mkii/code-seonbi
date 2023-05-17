039e3d86cf2e02743f303b648c28a8f137ec7883
ak89mkii <ak89mkii@gmail.com>
2023-05-17 11:09:39 -0400

    Adding dark mode localStorage.

--- frontend/src/pages/Django/Django.jsx
+++ frontend/src/pages/Django/Django.jsx
@@ -50,13 +50,7 @@ class Django extends Component {
     // Save mode: light in local Storage:
     handleFormSubmitLight = () => {
         localStorage.setItem('mode', 'light');
-        localStorage.setItem('photo', 'photo');
-        localStorage.setItem('nav', 'nav');
-        localStorage.setItem('menu', 'ui menu');
-        localStorage.setItem('contact', 'contact');
-        // localStorage.setItem('s1TitleSub', 's1TitleSub');
-        // localStorage.setItem('s2', 's2');
-        // localStorage.setItem('s2TitleSub', 's2TitleSub');
+        localStorage.setItem('mode2', 'darkNoText');
         localStorage.setItem('icon', sun);
     };
 
