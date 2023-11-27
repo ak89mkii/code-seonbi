04d8ce8ce4c927cafc57d21bca1dfbc6eee2c4e1
ak89mkii <ak89mkii@gmail.com>
2023-11-27 14:43:48 -0500

    Removed quotes from Bug page.

--- frontend/src/pages/Bug/Bug.jsx
+++ frontend/src/pages/Bug/Bug.jsx
@@ -113,7 +113,7 @@ class Bug extends Component {
                                     </div>
                                     </Card.Body>
                                     {/* <Card.Img variant="bottom" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto,c_pad,dpr_3.0,f_auto,q_auto,w_500/b_rgb:ffffff/v1/ncom/en_US/games/switch/s/sonic-mania-switch/hero" /> */}
-                                    <Card.Footer className="text-center">{JSON.stringify(list.id)}</Card.Footer>
+                                    <Card.Footer className="text-center">{JSON.stringify(list.id + 177)}</Card.Footer>
                                 </Card>
                             </Row>
                         </Container>
