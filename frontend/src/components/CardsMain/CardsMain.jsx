a4115ce4acc30606efd571efaa2ad811794b7b2e
Alex Kasem <ig-data@Alexs-Mac-mini.local>
2025-07-19 18:06:58 -0400

    Corrected the spelling of 'efficient' from 'efficicent'.

--- frontend/src/components/CardsMain/CardsMain.jsx
+++ frontend/src/components/CardsMain/CardsMain.jsx
@@ -30,7 +30,7 @@ const CardsMain = (props) => {
                                 <Card.Body>
                                     <Card.Title><div className='title2'>Bug Tracker</div></Card.Title>
                                     <Card.Text>
-                                        Keep track of your programming bugs in an organized and efficicent manner.
+                                        Keep track of your programming bugs in an organized and efficient manner.
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
